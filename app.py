5ad0d2ba1c621258d1004137eb3ea5195474fca8
nahucroco <102126123+nahucroco@users.noreply.github.com>
2026-07-06 14:13:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask, request
-from datetime import datetime
-import json
-import requests
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 
-URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzbF6FkoiQn_NRM4z65D3w-cXkOYvl7EHmmcM76RoTIbkCvXrd7S-zypseSo26saIzb/exec"
-
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+database_url = os.getenv("DATABASE_URL")
+
+# Render entrega una URL que empieza con postgres://
+# SQLAlchemy espera postgresql://
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 class Visita(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fecha = db.Column(db.String(50))
-    ip = db.Column(db.String(50))
+    fecha = db.Column(db.String(50), nullable=False)
+    ip = db.Column(db.String(50), nullable=False)
     user_agent = db.Column(db.Text)
+
 
 with app.app_context():
     db.create_all()
+
 
 @app.route("/")
 def inicio():
@@ -38,7 +45,7 @@ def inicio():
         ip=ip,
         user_agent=request.headers.get("User-Agent")
     )
-    
+
     db.session.add(visita)
     db.session.commit()
 
@@ -46,6 +53,20 @@ def inicio():
     <h2>Conexión realizada correctamente.</h2>
     <p>Puedes cerrar esta página.</p>
     """
+
+@app.route("/visitas")
+def visitas():
+
+    registros = Visita.query.order_by(Visita.id.desc()).all()
+
+    return [
+        {
+            "fecha": r.fecha,
+            "ip": r.ip,
+            "user_agent": r.user_agent
+        }
+        for r in registros
+    ]
 
 if __name__ == "__main__":
     app.run(debug=True)
